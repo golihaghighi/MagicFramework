ba1f498f27d9e2fc70394e3500b63e2625216bae
Afshin <Afshin@DESKTOP-MC6FBKH>
2020-11-29 22:04:23 +0330

    Layout 1

---
+++ lib/layout1/MagicFieldType.php
@@ -0,0 +1,13 @@
+<?php
+namespace lib\layout1;
+
+/** 
+ * @author Afshin
+ * @package MagicFramework
+ * @version 1.0
+ */
+abstract class MagicFieldType {
+
+	abstract public function render( $name, $label="",$description="", $options = array(), $args = array(), $hidden = false );
+}
+
---
+++ lib/layout1/iMagicRender.php
@@ -0,0 +1,15 @@
+<?php
+namespace lib\layout1;
+
+/** 
+ * A interface that implements Render methods
+ * @author Afshin
+ * @version 1.0
+ * @package MagicFramework
+ */
+interface iMagicRender {
+
+	public function render( $factory );
+}
+
+
